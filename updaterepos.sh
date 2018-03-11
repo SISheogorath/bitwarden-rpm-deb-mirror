3bb0af7de1cc54230c156d4db3eacbc4f5f4d3ae
Sheogorath <sheogorath@shivering-isles.com>
2018-03-11 12:00:30 +0100

    Add signing package process

--- updaterepos.sh
+++ updaterepos.sh
@@ -16,6 +16,20 @@ echo -e "Download new DEB packages"
 mkdir pkgs/debs/
 curl -L 'https://github.com/bitwarden/desktop/releases/download/v1.0.5/Bitwarden-1.0.5-amd64.deb' -o /tmp/bitwarden.deb
 
+# extract the public and private GPG keys from encrypted archive keys.tar with
+# the secret openssl pass KEY, which is stored in TravisCI variables
+echo -e "Import GPG keys"
+openssl enc -aes-256-cbc -d -md sha256 -a -in signing.key.enc -out signing.key -k "$KEY"
+#signing the repository metadata with my personal GPG key
+gpg --import pub.gpg && gpg --import signing.key
+
+
+# Sign RPM package
+echo "%_gpg_name $KEYID" > ~/.rpmmacros
+./rpmsign.exp pkgs/rpms/bitwarden.rpm "$KEY"
+
+# Sign deb package
+ debsigs --sign=origin -k $KEYID /tmp/bitwarden.deb
 
 # generation of new Deb repository from downloaded packages
 mkdir pkgs/debs/conf
@@ -26,11 +40,7 @@ echo 'Architectures: amd64' >> pkgs/debs/conf/distributions
 reprepro -b pkgs/debs includedeb bitwarden /tmp/*.deb
 
 echo "Sign the repositories"
-# extract the public and private GPG keys from encrypted archive keys.tar with
-# the secret openssl pass KEY, which is stored in TravisCI variables
-openssl enc -aes-256-cbc -d -md sha256 -a -in signing.key.enc -out signing.key -k "$KEY"
-#signing the repository metadata with my personal GPG key
-gpg --import pub.gpg && gpg --passphrase "$KEY" --import signing.key
+
 gpg --detach-sign --armor pkgs/rpms/repodata/repomd.xml
 gpg --detach-sign --armor pkgs/debs/dists/bitwarden/main/binary-amd64/Release
 
